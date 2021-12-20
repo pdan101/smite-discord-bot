f8d82648bc44120f32f1f64caf609007d42d2766
Prithwish Dan <dan.prithwish@gmail.com>
2021-12-19 22:01:19 -0500

    Finished event handling tutorial

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const { Client, Intents } = require('discord.js')
+const fs = require('fs')
+const { Client, Intents, Collection } = require('discord.js')
 const { token } = require('./config.json')
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-  console.log('Ready!')
-})
+const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'))
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`)
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+}
 
-	const { commandName } = interaction;
+client.commands = new Collection()
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'))
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	}
-});
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command)
+}
 
 // Login to Discord with your client's token.
 client.login(token)
