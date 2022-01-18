d6f16d8ec66c7aefcc0ec2352012ea7b590c6dd5
Miranda Luo <miranda.luo24@gmail.com>
2022-01-17 20:25:47 -0500

    added achievements

--- selections/playerAchievements.js
+++ selections/playerAchievements.js
@@ -5,7 +5,7 @@ module.exports = {
   getPlayerAchievements: async function getPlayerAchievements(playerId) {
     const achievements = await makeRequest('getplayerachievements', [playerId]);
 
-    console.log(achievements);
+    // console.log(achievements);
 
     const embed = new MessageEmbed()
       .setColor('#0099ff')
@@ -13,16 +13,47 @@ module.exports = {
       .addFields(
         {
           name: 'Kills',
-          value: achievements['AssistedKills'].toString(),
+          value: achievements['PlayerKills'].toString(),
           inline: true,
         },
         { name: 'Deaths', value: achievements['Deaths'].toString(), inline: true },
         {
           name: 'Assists',
-          value: achievements['PlayerKills'].toString(),
+          value: achievements['AssistedKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'First Bloods',
+          value: achievements['FirstBloods'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Shutdowns',
+          value: achievements['ShutdownSpree'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Double Kills',
+          value: achievements['DoubleKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Triple Kills',
+          value: achievements['TripleKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Quadra Kills',
+          value: achievements['QuadraKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Penta Kills',
+          value: achievements['PentaKills'].toString(),
           inline: true,
         },
         { name: '\u200B', value: '\u200B' },
+        { name: 'Killing Sprees', value: '\u200B' },
         {
           name: 'Killing Sprees',
           value: achievements['KillingSpree'].toString(),
@@ -51,6 +82,38 @@ module.exports = {
         {
           name: 'Godlikes',
           value: achievements['GodLikeSpree'].toString(),
+          inline: true,
+        },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Objective Kills', value: '\u200B' },
+        {
+          name: 'Gold Fury',
+          value: achievements['GoldFuryKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Fire Giant',
+          value: achievements['FireGiantKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Tower Kill',
+          value: achievements['TowerKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Phoenix Kill',
+          value: achievements['PhoenixKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Siege Juggernaut',
+          value: achievements['SiegeJuggernautKills'].toString(),
+          inline: true,
+        },
+        {
+          name: 'Wild Juggernaut',
+          value: achievements['WildJuggernautKills'].toString(),
           inline: true,
         }
       );
